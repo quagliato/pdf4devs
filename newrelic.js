68888b227aa4c902786e0d8b80401d94c02aa1a9
Eduardo Quagliato <eduardo@quagliato.me>
2015-12-08 14:47:27 -0200

    New relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['pdf4devs'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '4b48baafdb396aeee54ec44ed138b3a93020f187',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
