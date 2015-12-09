8a75cfac2866ce54fc637482f9cde93c30c56ac7
Eduardo Quagliato <eduardo@quagliato.me>
2015-12-09 00:10:29 -0200

    Require fs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('newrelic');
 
 bodyParser               = require("./node_modules/body-parser/index.js");
 express                  = require("./node_modules/express/index.js");
-fs                       = requite("fs");
+fs                       = require("fs");
 moment                   = require("./node_modules/moment/moment.js");
 pdfFromURL               = require("./pdf-from-url.js");
 sha1                     = require("./node_modules/sha1/sha1.js");
